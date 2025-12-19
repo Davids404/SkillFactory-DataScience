8e13659e642ef1121ba0a8dbcc7cbfb003aebe52
Davids001 <ronaz@mail.ru>
2025-12-19 14:56:57 +0300

    1

--- второй_год_обучения/app.py
+++ второй_год_обучения/app.py
@@ -1,5 +1,6 @@
-#Импортируем необходимые библиотеки
+# Импортируем необходимые библиотеки
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import streamlit as st
@@ -13,15 +14,17 @@ import plotly.express as px
 # -----------------------------------------------------------------------------------------------------
 # Реализуем функции необходимые для работы приложения
 
+
 @st.cache
-def read_files():
+def read_files(csv_file_1="ratings.csv", csv_file_2="books.csv"):
     """
     Функция для чтения файлов.
     Возвращает два DataFrame с рейтингами и характеристиками книг.
     """
-    ratings = pd.read_csv('ratings.csv')
-    books = pd.read_csv('books.csv')
-    return ratings, books 
+    ratings = pd.read_csv(csv_file_1)
+    books = pd.read_csv(csv_file_2)
+    return ratings, books
+
 
 def make_mappers(books):
     """
@@ -35,19 +38,22 @@ def make_mappers(books):
 
     return name_mapper, author_mapper
 
-def load_embeddings(file_name='item_embeddings.pkl'):
+
+def load_embeddings(file_name="item_embeddings.pkl"):
     """
     Функция для загрузки векторных представлений.
     Возвращает прочитанные эмбеддинги книг и индекс (граф) для поиска похожих книг.
     """
-    with open(file_name, 'rb') as f:
+    with open(file_name, "rb") as f:
         item_embeddings = pickle.load(f)
 
     # Тут мы используем nmslib, чтобы создать наш быстрый knn
-    nms_idx = nmslib.init(method='hnsw', space='cosinesimil')
+    nms_idx = nmslib.init(method="hnsw", space="cosinesimil")
     nms_idx.addDataPointBatch(item_embeddings)
     nms_idx.createIndex(print_progress=True)
     return item_embeddings, nms_idx
+
+
 # index = nms_idx
 def nearest_books_nms(book_id, index, n=10):
     """
@@ -56,6 +62,7 @@ def nearest_books_nms(book_id, index, n=10):
     """
     nn = index.knnQuery(item_embeddings[book_id], k=n)
     return nn
+
 
 def get_recomendation_df(ids, distances, name_mapper, author_mapper):
     """
@@ -67,20 +74,23 @@ def get_recomendation_df(ids, distances, name_mapper, author_mapper):
     """
     names = []
     authors = []
-    #Для каждого индекса книги находим ее название и автора
-    #Результаты добавляем в списки
+    # Для каждого индекса книги находим ее название и автора
+    # Результаты добавляем в списки
     for idx in ids:
         names.append(name_mapper[idx])
         authors.append(author_mapper[idx])
-    #Составляем DataFrame
-    recomendation_df = pd.DataFrame({'book_name': names, 'book_author': authors, 'distance': distances})
+    # Составляем DataFrame
+    recomendation_df = pd.DataFrame(
+        {"book_name": names, "book_author": authors, "distance": distances}
+    )
     return recomendation_df
 
-#Загружаем данные
-ratings, books = read_files() 
-#Создаем словари для сопоставления id книг и их названий/авторов
+
+# Загружаем данные
+ratings, books = read_files()
+# Создаем словари для сопоставления id книг и их названий/авторов
 name_mapper, author_mapper = make_mappers(books)
-#Загружаем эмбеддинги и создаем индекс для поиска
+# Загружаем эмбеддинги и создаем индекс для поиска
 item_embeddings, nms_idx = load_embeddings()
 
 # -----------------------------------------------------------------------------------------------------
@@ -100,47 +110,46 @@ To use the application, you need:
 After that, the application will give you a list of books most similar to the book you specified""")
 
 # Вводим строку для поиска книг
-title = st.text_input('Please enter book name')
+title = st.text_input("Please enter book name")
 title = title.lower()
 
-#Выполняем поиск по книгам - ищем неполные совпадения
-output = books[books['title'].apply(lambda x: x.lower().find(title)) >= 0]
+# Выполняем поиск по книгам - ищем неполные совпадения
+output = books[books["title"].apply(lambda x: x.lower().find(title)) >= 0]
 
-#Выбор книги из списка
-option = st.selectbox("Select the book you need", output['title'].values)
+# Выбор книги из списка
+option = st.selectbox("Select the book you need", output["title"].values)
 
-#Проверяем, что поле не пустое
+# Проверяем, что поле не пустое
 if option:
-    #Выводим выбранную книгу
+    # Выводим выбранную книгу
     st.markdown('You selected: "{}"'.format(option))
-    
-    #Находим book_id для указанной книги
-    val_index = output[output['title'].values == option]['book_id'].values
-    
-    #Указываем количество рекомендаций
+
+    # Находим book_id для указанной книги
+    val_index = output[output["title"].values == option]["book_id"].values
+
+    # Указываем количество рекомендаций
     count_recomendation = st.number_input(
-        label="Specify the number of recommendations you need", 
-        value=10
-    )    
-    #Находим count_recomendation+1 наиболее похожих книг
-    ids, distances = nearest_books_nms(val_index, nms_idx, count_recomendation+1)
-    #Убираем из результатов книгу, по которой производился поиск
+        label="Specify the number of recommendations you need", value=10
+    )
+    # Находим count_recomendation+1 наиболее похожих книг
+    ids, distances = nearest_books_nms(val_index, nms_idx, count_recomendation + 1)
+    # Убираем из результатов книгу, по которой производился поиск
     ids, distances = ids[1:], distances[1:]
-    
-    #Выводим рекомендации к ней
-    st.markdown('Most simmilar books are: ')
-    #Составляем DataFrame из рекомендаций
+
+    # Выводим рекомендации к ней
+    st.markdown("Most simmilar books are: ")
+    # Составляем DataFrame из рекомендаций
     df = get_recomendation_df(ids, distances, name_mapper, author_mapper)
-    #Выводим DataFrame в интерфейсе
-    st.dataframe(df[['book_name', 'book_author']])
-    
+    # Выводим DataFrame в интерфейсе
+    st.dataframe(df[["book_name", "book_author"]])
+
     # Строим столбчатую диаграмму
     fig = px.bar(
-        data_frame=df, 
-        x='book_name', 
-        y='distance',
-        hover_data=['book_author'],
-        title='Cosine distance to the nearest books'
+        data_frame=df,
+        x="book_name",
+        y="distance",
+        hover_data=["book_author"],
+        title="Cosine distance to the nearest books",
     )
     # Отображаем график в интерфейсе
     st.write(fig)
